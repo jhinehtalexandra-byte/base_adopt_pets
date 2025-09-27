decc673f92ae5f5515ebac2dda392cd06a4c5d53
Andres <andrescastibla@hotmail.com>
2025-09-27 07:51:06 -0500

    correcciones varias

--- resources/views/Refugios/refugios-admin.blade.php
+++ resources/views/Refugios/refugios-admin.blade.php
@@ -8,59 +8,48 @@
 
     <div class="py-12">
         <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-x1 sm:rounded-lg">
-                <div style="padding:16px">
-                    <div class="container">
-                        <div class="header-section">
-                            <h1 style="color: #137035;">Lista de Refugios</h1>
-                            <a href="{{ route('refugios-admin.create') }}" class="btn-formulario">
-                                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                                </svg>
-                                Registrar Nuevo Refugio
-                            </a>
-                        </div>
-                       
-                        <table id="refugios" class="display" style="width:100%; padding: 30px;" border="1" cellpadding="8" cellspacing="0" width="100%">
-                            <thead style="background: #f1fdf5">
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Direccion</th>
-                                    <th>Correo</th>
-                                    <th>Telefono</th>
-                                    <th>Responsable</th>
-                                    <th>Localidad</th>
-                                    <th>Capacidad</th>
-                                    <th>Fecha de Registro</th>
-                                    <th>Acciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($refugios as $refugio)
-                                    <tr>
-                                        <td>{{ $refugio->nombre_refugio}}</td>
-                                        <td>{{ $refugio->direccion}}</td>
-                                        <td>{{ $refugio->email}}</td>
-                                        <td>{{ $refugio->telefono}}</td>
-                                        <td>{{ $refugio->responsable}}</td>
-                                        <td>{{ $refugio->localidad}}</td>
-                                        <td>{{ $refugio->capacidad_maxima}}</td>
-                                        <td>{{ $refugio->fecha_registro}}</td>
-                                        <td class="flex gap-2">
-                                            <a href="{{ route('refugios-admin.edit', $refugio) }}" class="bg-green-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 mr-4">
-                                                Editar</a>
-                                            <form action="{{ route('refugios-admin.destroy', $refugio) }}" method="POST"
-                                                onsubmit="return confirm('¿Eliminar?')">
-                                                @csrf 
-                                                @method('DELETE')
-                                                <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 mr-4">Eliminar</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6 mb-10">
+                <div class="header-section">
+                    <h1 style="color: #137035;font-weight: bold; font-size: large;">Lista de Refugios</h1>
+                    <a href="{{ route('refugios-admin.create') }}" class="btn-formulario">Registrar Nuevo Refugio</a>
                 </div>
+                <table id="refugios" class="display w-full" border="1" cellpadding="8" cellspacing="0">
+                    <thead style="background: #f1fdf5">
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Direccion</th>
+                            <th>Correo</th>
+                            <th>Telefono</th>
+                            <th>Responsable</th>
+                            <th>Localidad</th>
+                            <th>Capacidad</th>
+                            <th>Fecha de Registro</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($refugios as $refugio)
+                        <tr>
+                            <td>{{ $refugio->nombre_refugio }}</td>
+                            <td>{{ $refugio->direccion }}</td>
+                            <td>{{ $refugio->email }}</td>
+                            <td>{{ $refugio->telefono }}</td>
+                            <td>{{ $refugio->responsable }}</td>
+                            <td>{{ $refugio->localidad }}</td>
+                            <td>{{ $refugio->capacidad_maxima }}</td>
+                            <td>{{ $refugio->fecha_registro }}</td>
+                            <td class="flex gap-2">
+                                <a href="{{ route('refugios-admin.edit', $refugio) }}" class="bg-green-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 mr-4">Editar</a>
+                                <form action="{{ route('refugios-admin.destroy', $refugio) }}" method="POST" onsubmit="return confirm('¿Eliminar?')">
+                                    @csrf 
+                                    @method('DELETE')
+                                    <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 mr-4">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div> 
     </div>
